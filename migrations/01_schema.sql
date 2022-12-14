fedd2243913c5f70d89dd28b7c9ef9df16e94223
Raeme Khatib <raeme.khatib@telus.com>
2022-12-14 09:56:57 -0700

    imported tables

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -15,12 +15,14 @@ CREATE TABLE properties (
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
   description TEXT,
+  
   thumbnail_photo_url VARCHAR(255) NOT NULL,
   cover_photo_url VARCHAR(255) NOT NULL,
   cost_per_night INTEGER  NOT NULL DEFAULT 0,
   parking_spaces INTEGER  NOT NULL DEFAULT 0,
   number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
   number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
+
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
